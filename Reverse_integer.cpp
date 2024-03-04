ad180567074dedd54f61eeee9352477eb9154580
Manasa <127980128+manasa0520@users.noreply.github.com>
2024-03-04 23:24:58 +0530

    Update Reverse_integer.cpp

--- Reverse_integer.cpp
+++ Reverse_integer.cpp
@@ -1,13 +1,15 @@
-class Solution {
-    public int reverse(int x) {
-        int rev = 0;
-        while (x != 0) {
-            int pop = x % 10;
-            x /= 10;
-            if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
-            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
-            rev = rev * 10 + pop;
-        }
-        return rev;
-    }
+class Solution { 
+ public: 
+     int reverse(int x) { 
+         int ans = 0; 
+          
+         while(x){ 
+             int pop = x%10; 
+             x /= 10; 
+              
+             if(ans > INT_MAX/10 || (ans == INT_MAX/10 && pop > 7)) 
+                 return 0; 
+             if(ans < INT_MIN/10 || (ans == INT_MIN/10 && pop < -8)) 
+                 return 0; 
+              
 }
