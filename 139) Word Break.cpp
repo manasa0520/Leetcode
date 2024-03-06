60b0d750f188cc67ff3fd5185977d33f24fe967c
Manasa <127980128+manasa0520@users.noreply.github.com>
2024-03-06 20:47:22 +0530

    Create 139) Word Break.cpp

---
+++ 139) Word Break.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n = s.size();
+        
+        //key: end index, 1-based
+        vector<bool> dp(n+1, false);
+        //empty string
+        dp[0] = true;
+        
+        for(int end = 1; end <= n; ++end){
+            for(int start = 0; start < end; ++start){
+                //dp[start]: s[0...start-1]
+                if(dp[start] && find(wordDict.begin(), wordDict.end(), s.substr(start, end-start)) != wordDict.end()){
+                    //s[0...end-1] can be split into s[0...start-1] and s[start...end-1]
+                    dp[end] = true;
+                    break;
+                }
+            }
+        }
+        
+        return dp[n];
+    }
+};
