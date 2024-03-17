56869f028d89b31ebe755689c2ef58d8932b3e7b
Manasa <127980128+manasa0520@users.noreply.github.com>
2024-03-17 12:21:27 +0530

    Create 57) Insert Interval.cpp

---
+++ 57) Insert Interval.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> ans;
+        int i = 0, n = intervals.size();
+        
+       
+        while(i < n && intervals[i][1] < newInterval[0]){
+            ans.push_back(intervals[i]);
+            ++i;
+        }
+        
+        int start = newInterval[0], end = newInterval[1];
+        while(i < n && intervals[i][0] <= end){
+            start = min(intervals[i][0], start);
+            end = max(intervals[i][1], end);
+            ++i;
+        }
+        
+        ans.push_back({start, end});
+        
+        while(i < n){
+            ans.push_back(intervals[i]);
+            ++i;
+        }
+        
+        return ans;
+    }
+};
