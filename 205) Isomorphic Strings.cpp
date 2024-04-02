03062898f5ced552d7e6834812e4de814e33aff9
Manasa <127980128+manasa0520@users.noreply.github.com>
2024-04-02 23:55:57 +0530

    Create 205) Isomorphic Strings.cpp

---
+++ 205) Isomorphic Strings.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        map <char, char> m1;
+        map <char, char> m2;
+        for(int i = 0; i < s.size(); i++){
+            if(m1.count(s[i]) && m1[s[i]] != t[i]) return false;
+            if(m2.count(t[i]) && m2[t[i]] != s[i]) return false;
+            m1[s[i]] = t[i];
+            m2[t[i]] = s[i];
+        }
+        return true;
+    }
+};
